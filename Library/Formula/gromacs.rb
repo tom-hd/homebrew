b2addc5a46a035cc7848725bad7c026ac358c9b1
Thomas Dunton <thomas.dunton@gmail.com>
2012-12-24 13:07:28 +0000

    Use GROMACS beta-2.
    Update gromacs formula to version 4.6-b2. Also, tidy up formula script and add
    a test.

--- Library/Formula/gromacs.rb
+++ Library/Formula/gromacs.rb
@@ -2,27 +2,31 @@ require 'formula'
 
 class Gromacs < Formula
   homepage 'http://www.gromacs.org/'
-  url 'http://www.scalalife.eu/system/files/Gromacs-4.6-ScalaLife2012.tgz'
-  sha1 '9f2d7b6bd6ffaee8ffd421d891785e682fabf3f2'
+  url 'ftp://ftp.gromacs.org/pub/gromacs/gromacs-4.6-beta2.tar.gz'
+  md5 'e2c393e6183a40fa734d150800c7e163'
+  version '4.6'
 
-  option 'enable-mpi', "Enables MPI support"
-  option 'enable-double',"Enables double precision"
-  option 'without-x', "Disable the X11 visualizer"
+  option 'enable-double', 'Enable double precision'
 
   depends_on :x11
   depends_on 'fftw'
   depends_on 'cmake'
 
   def install
-    #args = ["-DCMAKE_INSTALL_PREFIX=#{prefix}"]
-    #args << "--enable-mpi" if build.include? 'enable-mpi'
-    #args << "--enable-double" if build.include? 'enable-double'
+    args = std_cmake_args
+    args << '-DGMX_GPU=OFF'
+    args << 'DGMX_OPENMP=OFF'
+    args << '-DGMX_DOUBLE=ON' if build.include? 'enable-double'
+    args << '..'
 
-    system "mkdir build"
-    system "cd build"
-    system "cmake ../gromacs-4.6-ScalaLife-120831 -DCMAKE_INSTALL_PREFIX=#{prefix} -DGMX_GPU=OFF -DGMX_OPENMP=OFF"
-    system "make -j2"
-    #ENV.j2
-    system "make install"
+    mkdir 'build' do
+      system 'cmake', *args
+      system 'make'
+      system 'make install'
+    end
+  end
+
+  def test
+    system "grompp -h"
   end
 end
